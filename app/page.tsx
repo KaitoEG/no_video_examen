3d3459f03baacd46dd0fc2edc09df8b47ad80e02
KaitoEG <eaghtspr@gmail.com>
2024-12-07 11:05:45 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Cabezera from "@/components/cabezera"; // Importa tu componente correctamente
+
+export default async function Home() {
+  return (
+    <div>
+      <Cabezera /> 
+    </div>
+  );
+}
---
+++ components/cabezera.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './index.module.css';
+
+const Cabezera = () => {
+  return (
+    <div className={styles.info}>
+      <b className={styles.title}>Exercícios</b>
+      <div className={styles.div}>4</div>
+    </div>
+  );
+};
+
+export default Cabezera;
